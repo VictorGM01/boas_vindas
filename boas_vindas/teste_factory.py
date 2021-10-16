39201b02900a6a33806fcc84870c2d4e5b24a180
VictorGM01 <victormarques8801@gmail.com>
2021-10-16 20:56:28 -0300

    adiciona janelas que já tinham sido criadas ao arquivo teste_factory

--- boas_vindas/teste_factory.py
+++ boas_vindas/teste_factory.py
@@ -47,11 +47,21 @@ class BoasVindas:
                 if valores['repositorios']:
                     return Repositorios()
 
+
 class Repositorios:
     def __init__(self):
-        janela1 = self.janela_repositorio_cybersecurity()
-        self.janela1 = janela1
+        # chama janelas
+        janela_cybersecurity = self.janela_repositorio_cybersecurity()
         self.chama_janelas()
+        janela_interfaces_graficas = None
+        janela_dicionario_html = None
+        janela_mais_repositorios = None
+
+        # referencia janelas
+        self.janela_interfaces_graficas = janela_interfaces_graficas
+        self.janela_dicionario_html = janela_dicionario_html
+        self.janela_mais_repositorios = janela_mais_repositorios
+        self.janela_cybersecurity = janela_cybersecurity
 
     @staticmethod
     def janela_repositorio_cybersecurity():
@@ -84,6 +94,103 @@ class Repositorios:
 
         return sg.Window('Repositórios', layout, element_justification='c',
                          finalize=True)
+
+    @staticmethod
+    def janela_repositorio_interfaces_graficas():
+        sg.theme('DarkBlack')
+        layout = [
+            [sg.Text('Interfaces Gráficas', font=('Times New Roman', 20)),
+             sg.Image(filename='icons\icon_interface.png')],
+            [sg.Text('Este repositório contém interfaces gráficas de ' +
+                     'códigos de outros repositórios, tornado-os ' +
+                     'em algo mais visual e intuitivo.')],
+            [sg.Text('Para as interfaces foi utilizado a biblioteca' +
+                     ' PySimpleGUI, o que deixou-as muito bonitas')],
+            [sg.Text('')],
+            [sg.Text('Conteúdo:')],
+            [sg.Image(filename='icons\interface1.png'),
+             sg.Listbox(['Dicionário de tags do HTML',
+                         'Gerador de senhas seguras'], size=(30, 1),
+                        font=('Times New Roman', 12)),
+             sg.Image(filename='icons\interface2.png')],
+            [sg.Text('')],
+            [sg.Button('', image_filename=r'icons\btn_icon_home.png',
+                       key='home'),
+             sg.Button('', image_filename=r'icons\btn_github.png', key='git'),
+             sg.Button('', image_filename='icons\clear.png', key='x'),
+             sg.Button('', image_filename=r'icons\btn_next.png', key='next')]
+        ]
+
+        return sg.Window('Interfaces Gráficas', layout,
+                         element_justification='c', finalize=True)
+
+    @staticmethod
+    def janela_repositorio_dicionario_html():
+        sg.theme('DarkBlack')
+        layout = [
+            [sg.Text('Dicionário HTML', font=('Times New Roman', 20)),
+             sg.Image(filename='icons\icon_html.png')],
+            [sg.Text('Este repositório é dedicado à Raphaela Ferraz.' +
+                     'Contém um script em Python que é capaz de adicionar e ' +
+                     'buscar tags do HTML5.')],
+            [sg.Text('Tem o intuito de facilitar a busca por tags específicas'
+                     + ', sendo alimentado conforme a Rapha vai'
+                     + ' aprendendo')],
+            [sg.Text('')],
+            [sg.Text('Conteúdo:')],
+            [sg.Image(filename='icons\icon_tags.png'),
+             sg.Listbox(['Dicionário de tags do HTML'], size=(27, 1),
+                        font=('Times New Roman', 12)),
+             sg.Image(filename='icons\icon_tags.png')],
+            [sg.Text('')],
+            [sg.Button('', image_filename=r'icons\btn_icon_home.png',
+                       key='home'),
+             sg.Button('', image_filename=r'icons\btn_github.png', key='git'),
+             sg.Button('', image_filename='icons\clear.png', key='x'),
+             sg.Button('', image_filename=r'icons\btn_next.png', key='next')]
+        ]
+
+        return sg.Window('Dicionário HTML', layout, element_justification='c',
+                         finalize=True)
+
+    @staticmethod
+    def janela_mais_repositorios():
+        sg.theme('DarkBlack')
+        layout = [
+            [sg.Text('Mais Repositórios', font=('Times New Roman', 20)),
+             sg.Image(filename='icons\icon_repo.png')],
+            [sg.Text('Ainda há muitos outros repositórios no meu perfil!' +
+                     ' Todos foram feitos em Python e abrangem ' +
+                     'assuntos distintos e muito interessantes',
+                     font=('Times New Roman', 13))],
+            [sg.Text('')],
+            [sg.Text('Automated Testings', size=(32, 1)),
+             sg.Button('', image_filename=r'icons\btn_ok.png',
+                       key='auto_tests')],
+            [sg.Text('Validate', size=(32, 1)),
+             sg.Button('', image_filename=r'icons\btn_ok.png',
+                       key='validate')],
+            [sg.Text('Automação Tabela Excel', size=(32, 1)),
+             sg.Button('', image_filename=r'icons\btn_ok.png',
+                       key='automacao_tabela')],
+            [sg.Text('Extrator URL', size=(32, 1)),
+             sg.Button('', image_filename=r'icons\btn_ok.png',
+                       key='extrator')],
+            [sg.Text('Playlist', size=(32, 1)),
+             sg.Button('', image_filename=r'icons\btn_ok.png',
+                       key='playlist')],
+            [sg.Text('Análise Descritiva PNAD COVID-19', size=(32, 1)),
+             sg.Button('', image_filename=r'icons\btn_ok.png',
+                       key='analise_pnad')],
+            [sg.Text('')],
+            [sg.Button('', image_filename=r'icons\btn_icon_home.png',
+                       key='home'),
+             sg.Button('', image_filename=r'icons\btn_github.png', key='git'),
+             sg.Button('', image_filename='icons\clear.png', key='x')]
+        ]
+
+        return sg.Window('Mais repositórios', layout, finalize=True,
+                         element_justification='c')
 
     def chama_janelas(self):
         while True:
